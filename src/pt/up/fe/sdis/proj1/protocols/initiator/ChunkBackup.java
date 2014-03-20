e0fe5ac1d70e187b1d1712b37309ecdf4dbc4c99
migulorama <migulorama@gmail.com>
2014-03-20 15:53:39 +0000

    Synchronize Chunk Backup Protocol.

--- src/pt/up/fe/sdis/proj1/protocols/initiator/ChunkBackup.java
+++ src/pt/up/fe/sdis/proj1/protocols/initiator/ChunkBackup.java
@@ -18,56 +18,50 @@ import rx.schedulers.Schedulers;
 public class ChunkBackup extends AbstractProtocol {
     public ChunkBackup(final Communicator comm, final Chunk chunk) {
         super(comm.MC.Publisher);
+        
+        final Message msg = Message.makePutChunk(chunk);
 
-        Schedulers.newThread().schedule(new Action1<Scheduler.Inner>() {
+        start(new Func1<Message, Boolean>() {
+            @Override
+            public Boolean call(Message arg0) {
+                return arg0.type == Message.Type.STORED
+                        && arg0.getFileID().equals(chunk.fileID)
+                        && arg0.getChunkNo().equals(chunk.chunkNo);
+            }
+        });
+
+        comm.MDB.Sender.Send(msg);
+
+        Action1<Scheduler.Inner> act = new Action1<Scheduler.Inner>() {
+            int TimeInterval = 500;
+            int NumTimes = 0;
 
             @Override
             public void call(Inner arg0) {
+                synchronized (_repliers) {
+                    NumTimes++;
 
-                final Message msg = Message.makePutChunk(chunk);
-                
-                start(new Func1<Message, Boolean>() {
-                    @Override
-                    public Boolean call(Message arg0) {
-                        return arg0.type == Message.Type.STORED
-                                && arg0.getFileID().equals(chunk.fileID)
-                                && arg0.getChunkNo().equals(chunk.chunkNo);
-                    }
-                });
-                
-                comm.MDB.Sender.Send(msg);
-                
-                Action1<Scheduler.Inner> act = new Action1<Scheduler.Inner>() {
-                    int TimeInterval = 500;
-                    int NumTimes = 0;
-                    
-                    @Override
-                    public void call(Inner arg0) {
-                        synchronized (_repliers) {
-                            NumTimes++;
-                            
-                            int numRepliers = _repliers.size();
-                            
-                            if (numRepliers < chunk.replicationDeg) {
-                                if (NumTimes == 5) {
-                                	//TODO replace with some kind of warning
-                                    finish();
-                                } else {
-                                    _repliers.clear();
-                                    TimeInterval *= 2;
-                                    comm.MDB.Sender.Send(msg);
-                                    arg0.schedule(this, TimeInterval, TimeUnit.MILLISECONDS);
-                                }
-                            } else {
-                                finish();
-                            }
+                    int numRepliers = _repliers.size();
+
+                    if (numRepliers < chunk.replicationDeg) {
+                        if (NumTimes == 5) {
+                            // TODO replace with some kind of warning
+                            finish();
+                        } else {
+                            _repliers.clear();
+                            TimeInterval *= 2;
+                            comm.MDB.Sender.Send(msg);
+                            arg0.schedule(this, TimeInterval,
+                                    TimeUnit.MILLISECONDS);
                         }
+                    } else {
+                        finish();
                     }
-                };
-                
-                arg0.schedule(act, 500, TimeUnit.MILLISECONDS);
+                }
             }
-        });
+        };
+
+        Schedulers.newThread().schedule(act, 500, TimeUnit.MILLISECONDS);
     }
 
     @Override
@@ -76,6 +70,6 @@ public class ChunkBackup extends AbstractProtocol {
             _repliers.add(msg.Sender);
         }
     }
-    
+
     private Set<InetAddress> _repliers = new HashSet<InetAddress>();
 }
