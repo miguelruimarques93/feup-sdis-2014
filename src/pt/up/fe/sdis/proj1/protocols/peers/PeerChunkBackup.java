cc5a0d81cdcc502418295b887fd0c026a6f2906d
migulorama <migulorama@gmail.com>
2014-03-29 00:23:37 +0000

    Implement replyCounter on PeerChunkBackup.

--- src/pt/up/fe/sdis/proj1/protocols/peers/PeerChunkBackup.java
+++ src/pt/up/fe/sdis/proj1/protocols/peers/PeerChunkBackup.java
@@ -8,8 +8,10 @@ import pt.up.fe.sdis.proj1.messages.Message;
 import pt.up.fe.sdis.proj1.protocols.AbstractProtocol;
 import pt.up.fe.sdis.proj1.protocols.initiator.SpaceReclaiming;
 import pt.up.fe.sdis.proj1.utils.Communicator;
+import pt.up.fe.sdis.proj1.utils.CounterObserver;
 import pt.up.fe.sdis.proj1.utils.MessageFilter;
 import rx.Scheduler;
+import rx.Subscription;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
@@ -29,10 +31,21 @@ public class PeerChunkBackup extends AbstractProtocol {
             new SpaceReclaiming(_bs, true);
 
         if(_bs.getAvailableSpace() >= msg.getBody().length){
+            final CounterObserver replyCounter = new CounterObserver();
+            final Subscription sub = _bs.Comm.MDR.Publisher.getObservable()
+                    .filter(new MessageFilter(Message.Type.STORED, msg.getFileID(), msg.getChunkNo())).subscribe(replyCounter);
+            
             Schedulers.io().schedule(new Action1<Scheduler.Inner>() {
                 @Override
                 public void call(Scheduler.Inner arg0) {
-                    _comm.MC.Sender.Send(Message.makeStored(msg.getFileID(), msg.getChunkNo()));
+                    int i = replyCounter.getNumReceived();
+                    if (i < msg.getReplicationDeg()) {
+                        _comm.MC.Sender.Send(Message.makeStored(msg.getFileID(), msg.getChunkNo()));
+                    } else {
+                        _bs.deleteChunk(msg.getFileID(), msg.getChunkNo());
+                        _bs.Files.removeChunk(msg.getFileID(), msg.getChunkNo());
+                    }
+                    sub.unsubscribe();
                 }
             }, rand.nextInt(401), TimeUnit.MILLISECONDS);
             
